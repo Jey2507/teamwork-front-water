e5daddfb5340352e6539bb46e0d775ca65621ec4
Nikita <nikitaefron06@gmail.com>
2024-08-03 02:52:16 +0200

    Summary (required)

---
+++ src/components/WaterModal/WaterModal.jsx
@@ -0,0 +1,91 @@
+import {useTranslation} from "react-i18next";
+import css from "./WaterModal.module.css";
+import WaterForm from "../WaterForm/WaterForm";
+import {ANIMATION} from "../../constants";
+import svgSprite from "../../assets/sprite.svg";
+
+const WaterModal = ({
+  operationType,
+  onClose,
+  water = {},
+  timestampFromUrl = "",
+}) => {
+  // operationType: "add" | "edit", onClose: () => void, water: {id: 'string', amount: number, date: "string"}
+  const {t} = useTranslation();
+
+  const handleClose = () => {
+    const id = setTimeout(() => {
+      onClose();
+      clearTimeout(id);
+    }, ANIMATION.DURATION);
+  };
+
+  const modalHeader = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return t("addWaterTitle");
+      case "edit":
+        return t("editWaterAmount");
+      default:
+        return t("addWaterTitle");
+    }
+  };
+
+  const curentTimestamp = Number(timestampFromUrl);
+  const recordTimestamp = Number(water.date);
+
+  const editTime = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return curentTimestamp;
+      case "edit":
+        return recordTimestamp;
+    }
+  };
+
+  const waterPortion = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return 50;
+      case "edit":
+        return water.amount;
+      default:
+        return 50;
+    }
+  };
+
+  const waterID = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return null;
+      case "edit":
+        return water.id;
+      default:
+        return null;
+    }
+  };
+
+  return (
+    <div className={css.WaterModal}>
+      <h1>{modalHeader(operationType)}</h1>
+      <WaterForm
+        operationType={operationType}
+        editTime={editTime(operationType)} // Передаємо мілісекунди
+        waterPortion={waterPortion(operationType)} // Передаємо порцію води
+        waterID={waterID(operationType)} // Передаємо ID води
+        handleClose={handleClose}
+      />
+      <button
+        type="button"
+        onClick={handleClose}
+        aria-label={t("closeWaterModal")}
+        className={css.WaterModalCloseBtn}>
+        <svg>
+          <use xlinkHref={svgSprite + "#icon-clear"}></use>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default WaterModal;
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App.jsx';
-import './index.css';
-import { Provider } from 'react-redux';
-import { BrowserRouter } from 'react-router-dom';
-import { store } from './redux/store.js';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./components/App/App.jsx";
+import "./index.css";
+import {Provider} from "react-redux";
+import {BrowserRouter} from "react-router-dom";
+import {store} from "./redux/store.js";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
