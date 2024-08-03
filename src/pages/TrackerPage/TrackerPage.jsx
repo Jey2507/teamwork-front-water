9418213b30a7848c532af9d8881ccd79779f149a
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-08-03 19:46:06 +0300

    Update TrackerPage.jsx

--- src/pages/TrackerPage/TrackerPage.jsx
+++ src/pages/TrackerPage/TrackerPage.jsx
@@ -1,37 +1,37 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Helmet } from 'react-helmet-async';
-import { selectLoading } from '../../redux/auth/selectors';
+// import { useEffect } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { Helmet } from 'react-helmet-async';
+// import { selectLoading } from '../../redux/auth/selectors';
 
-import { getUser } from '../../redux/auth/operations';
-// import { selectLoading } from '../../redux/water/selectors';
+// import { getUser } from '../../redux/auth/operations';
+// // import { selectLoading } from '../../redux/water/selectors';
 
-// import Loader from '../../components/Loader/Loader';
-// import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
-// import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
+// // import Loader from '../../components/Loader/Loader';
+// // import WaterMainInfo from '../../components/WaterMainInfo/WaterMainInfo';
+// // import WaterDetailedInfo from '../../components/WaterDetailedInfo/WaterDetailedInfo';
 
-// import css from './TrackerPage.module.css';
+// // import css from './TrackerPage.module.css';
 
-const TrackerPage = () => {
-  const dispatch = useDispatch();
-  const loading = useSelector(selectLoading);
+// const TrackerPage = () => {
+//   const dispatch = useDispatch();
+//   const loading = useSelector(selectLoading);
 
-  useEffect(() => {
-    dispatch(getUser());
-  }, [dispatch]);
+//   useEffect(() => {
+//     dispatch(getUser());
+//   }, [dispatch]);
 
-  return (
-    <section>
-      <Loader loading={loading} />
-      <div className={css.trackContainer}>
-        <Helmet>
-          <title>AQUATRACK</title>
-        </Helmet>
-        <WaterMainInfo />
-        <WaterDetailedInfo />
-      </div>
-    </section>
-  );
-};
+//   return (
+//     <section>
+//       <Loader loading={loading} />
+//       <div className={css.trackContainer}>
+//         <Helmet>
+//           <title>AQUATRACK</title>
+//         </Helmet>
+//         <WaterMainInfo />
+//         <WaterDetailedInfo />
+//       </div>
+//     </section>
+//   );
+// };
 
-export default TrackerPage;
+// export default TrackerPage;
