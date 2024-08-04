d5011b66f0d83587550243fdb45abd9de25a1284
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-08-04 23:00:36 +0200

    redirect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from "react-router-dom";
-import { lazy } from "react";
+import { lazy, Suspense } from "react";
 
 const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
 const NotFoundPage = lazy(() =>
@@ -13,13 +13,15 @@ const SignUpPage = lazy(() => import("../../pages/SignUpPage/SignUpPage.jsx"));
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/signin" element={<SignInPage />} />
-      <Route path="/signup" element={<SignUpPage />} />
-      <Route path="/tracker" element={<TrackerPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <Suspense>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/tracker" element={<TrackerPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </Suspense>
   );
 }
 
