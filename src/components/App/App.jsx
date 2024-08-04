202e4136b1f0e2d2c2f22c3717ff48fa12814593
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-08-04 12:21:42 +0200

    signUp - skelet

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,16 +1,22 @@
-import { Route, Routes } from 'react-router-dom';
-import { lazy } from 'react';
+import { Route, Routes } from "react-router-dom";
+import { lazy } from "react";
 
-const HomePage = lazy(() => import('../../pages/HomePage/HomePage.jsx'));
-const NotFoundPage = lazy(() => import('../../pages/NotFoundPage/NotFoundPage.jsx'));
-const TrackerPage = lazy(() => import('../../pages/TrackerPage/TrackerPage.jsx'));
-const SignInPage = lazy(() => import('../../pages/SignInPage/SignInPage.jsx'));
+const HomePage = lazy(() => import("../../pages/HomePage/HomePage.jsx"));
+const NotFoundPage = lazy(() =>
+  import("../../pages/NotFoundPage/NotFoundPage.jsx")
+);
+const TrackerPage = lazy(() =>
+  import("../../pages/TrackerPage/TrackerPage.jsx")
+);
+const SignInPage = lazy(() => import("../../pages/SignInPage/SignInPage.jsx"));
+const SignUpPage = lazy(() => import("../../pages/SignUpPage/SignUpPage.jsx"));
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<HomePage />} />
       <Route path="/signin" element={<SignInPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
       <Route path="/tracker" element={<TrackerPage />} />
       <Route path="*" element={<NotFoundPage />} />
     </Routes>
