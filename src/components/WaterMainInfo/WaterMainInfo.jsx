5f7b87aa9e52fc7324818b9290c4fc0ffb783447
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-08-05 18:01:29 +0300

    Update WaterMainInfo.jsx

--- src/components/WaterMainInfo/WaterMainInfo.jsx
+++ src/components/WaterMainInfo/WaterMainInfo.jsx
@@ -1,23 +1,34 @@
 // import Logo from "../Logo/Logo";
-import WaterDailyNorma from "./WaterDailyNorma/WaterDailyNorma.jsx";
-import WaterProgressBar from "./WaterProgressBar/WaterProgressBar.jsx";
-import AddWaterButton from "./AddWaterButton/AddWaterButton.jsx";
-import { tp1xdt, tp2xdt, tp3xdt, tp1xtb, tp2xtb, tp3xtb, tp1xmb,tp2xmb,tp3xmb } from "../../images/TrackerPage/index.js";
+import WaterDailyNorma from './WaterDailyNorma/WaterDailyNorma.jsx';
+import WaterProgressBar from './WaterProgressBar/WaterProgressBar.jsx';
+import AddWaterButton from './AddWaterButton/AddWaterButton.jsx';
+import {
+  tp1xdt,
+  tp2xdt,
+  tp3xdt,
+  tp1xtb,
+  tp2xtb,
+  tp3xtb,
+  tp1xmb,
+  tp2xmb,
+  tp3xmb,
+} from '../../images/TrackerPage/index.js';
 
-
-import css from "./WaterMainInfo.module.css"
+import css from './WaterMainInfo.module.css';
 
 export default function WaterMainInfo() {
-    return (
-        <section className={css.container}>
-            {/* Потім замість цього буде компонент ЛОГО */}
-            <div className={css.logo} >AQUATRACK</div>
-            <img srcSet={`${tp1xmb} 262w, ${tp1xtb} 374w, ${tp1xdt} 472w, ${tp2xmb} 524w, ${tp2xtb} 748w, ${tp3xmb} 786w, ${tp2xdt} 944w, ${tp3xtb} 1122w, ${tp3xdt} 1416w`}
-                src={tp1xmb} alt="" />
-            <WaterDailyNorma />
-            <WaterProgressBar />
-            <AddWaterButton/>
-            
-        </section>
-    )
-};
+  return (
+    <section className={css.container}>
+      {/* Потім замість цього буде компонент ЛОГО */}
+      <div className={css.logo}>AQUATRACK</div>
+      <img
+        srcSet={`${tp1xmb} 262w, ${tp1xtb} 374w, ${tp1xdt} 472w, ${tp2xmb} 524w, ${tp2xtb} 748w, ${tp3xmb} 786w, ${tp2xdt} 944w, ${tp3xtb} 1122w, ${tp3xdt} 1416w`}
+        src={tp1xmb}
+        alt=""
+      />
+      <WaterDailyNorma />
+      <WaterProgressBar />
+      <AddWaterButton />
+    </section>
+  );
+}
