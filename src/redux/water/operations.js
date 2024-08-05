e4502d1d907293fc6700cfdb84482a9ae1c66d29
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-08-05 17:28:06 +0300

    Update operations.js

--- src/redux/water/operations.js
+++ src/redux/water/operations.js
@@ -1,37 +1,36 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
-import { addWaterReq, getWaterDayReq, getWaterMonthReq} from "./services";
+import { addWaterReq, getWaterDayReq, getWaterMonthReq } from "./services.js";
 
 export const addWater = createAsyncThunk(
-    'water/addWater',
-    async (newEntry, thunkAPI) => {
-        try {
-            const response = await addWaterReq(newEntry);
-            return response.data;
-        } catch (error) {
-            return thunkAPI.rejectWithValue(error.message);
-        }
+  'water/addWater',
+  async (newEntry, thunkAPI) => {
+    try {
+      const response = await addWaterReq(newEntry);
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
+  }
 );
 export const getWaterDay = createAsyncThunk(
-    'water/getWaterDay',
-    async (day, thunkAPI) => {
-      try {
-        const response = await getWaterDayReq(day);
-        return response;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.message);
-      }
+  'water/getWaterDay',
+  async (day, thunkAPI) => {
+    try {
+      const response = await getWaterDayReq(day);
+      return response;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
-  );
-  export const getWaterMonth = createAsyncThunk(
-    'water/getWaterMonth',
-    async (date, thunkAPI) => {
-      try {
-        const response = await getWaterMonthReq(date);
-        return response.daysInMonth;
-      } catch (error) {
-        return thunkAPI.rejectWithValue(error.message);
-      }
+  }
+);
+export const getWaterMonth = createAsyncThunk(
+  'water/getWaterMonth',
+  async (date, thunkAPI) => {
+    try {
+      const response = await getWaterMonthReq(date);
+      return response.daysInMonth;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
     }
-  );
-  
+  }
+);
