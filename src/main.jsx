8b1ae46d71faf423c330ec3f06303ad189e31a6e
Nikita <nikitaefron06@gmail.com>
2024-08-06 18:44:11 +0200

    11

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./components/App/App.jsx";
-import "./index.css";
-import {Provider} from "react-redux";
-import {BrowserRouter} from "react-router-dom";
-import {store} from "./redux/store.js";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './components/App/App.jsx';
+import './index.css';
+import {Provider} from 'react-redux';
+import {BrowserRouter} from 'react-router-dom';
+import {store} from './redux/store.js';
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
