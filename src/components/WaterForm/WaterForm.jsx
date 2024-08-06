5d232f0d2cc58662d2c410a694adfdc778efb552
Nikita <nikitaefron06@gmail.com>
2024-08-06 13:32:20 +0200

    Final view

--- src/components/WaterForm/WaterForm.jsx
+++ src/components/WaterForm/WaterForm.jsx
@@ -1,43 +1,200 @@
+import {useState} from "react";
+import {useForm, Controller} from "react-hook-form";
+import {yupResolver} from "@hookform/resolvers/yup";
+import * as Yup from "yup";
 import css from "./WaterForm.module.css";
 import clsx from "clsx";
+import svgSprite from "../../assets/sprite.svg";
+import {useDispatch} from "react-redux";
+import {addWater, updateWaterIntakeRecord} from "../../redux/water/operations";
 
-const WaterForm = ({operationType}) => {
+const WaterForm = ({
+  operationType = "add",
+  editTime,
+  waterPortion,
+  waterID,
+  handleClose,
+}) => {
+  const [waterAmount, setWaterAmount] = useState(waterPortion);
+  const dispatch = useDispatch();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const dateFromUrl = new Date(editTime);
+
+  const year = dateFromUrl.getFullYear();
+  const month = String(dateFromUrl.getMonth() + 1).padStart(2, "0");
+  const day = String(dateFromUrl.getDate()).padStart(2, "0");
+
+  const currentTime = operationType === "add" ? new Date() : dateFromUrl;
+  const hours = String(currentTime.getHours()).padStart(2, "0");
+  const minutes = String(currentTime.getMinutes()).padStart(2, "0");
+
+  const [formHours, setFormHours] = useState(hours);
+  const [formMinutes, setFormMinutes] = useState(minutes);
+
+  const validationSchema = Yup.object().shape({
+    recordingTime: Yup.string()
+      .required(t("recordTimeRequired"))
+      .matches(/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/, t("invalidTimeFormat")),
+    waterValue: Yup.number()
+      .required(t("waterValueRequired"))
+      .min(50, t("waterValueGreater"))
+      .max(5000, t("waterValueLess")),
+  });
+
+  const {
+    control,
+    handleSubmit,
+    setValue,
+    formState: {errors},
+  } = useForm({
+    resolver: yupResolver(validationSchema),
+    defaultValues: {
+      recordingTime: `${formHours}:${formMinutes}`,
+      waterValue: waterAmount.toString(),
+    },
+  });
+
+  const onSubmit = (data) => {
+    const combinedDateTime = new Date(
+      `${year}-${month}-${day}T${formHours}:${formMinutes}:00`
+    );
+    const timeToSend = combinedDateTime.getTime().toString();
+
+    const addWaterValue = {
+      amount: data.waterValue,
+      date: timeToSend,
+    };
+
+    const editWaterValue = {
+      amount: data.waterValue,
+      date: timeToSend,
+    };
+
+    setIsLoading(true);
+
+    switch (operationType) {
+      case "add":
+        dispatch(addWater(addWaterValue)).then(({error}) => {
+          if (!error) {
+            setIsLoading(false);
+            handleClose();
+          } else {
+            setIsLoading(false);
+          }
+        });
+        break;
+      case "edit":
+        dispatch(
+          updateWaterIntakeRecord({id: waterID, formData: editWaterValue})
+        ).then(({error}) => {
+          if (!error) {
+            setIsLoading(false);
+            handleClose();
+          } else {
+            setIsLoading(false);
+          }
+        });
+        break;
+      default:
+        setIsLoading(false);
+        break;
+    }
+  };
+
   const FormHeader = (operationType) => {
     switch (operationType) {
       case "add":
-        return <p className={css.FormHeader}>Choose a value</p>;
+        return <p className={css.FormHeader}>{t("chooseValue")}</p>;
       case "edit":
-        return <p className={css.FormHeader}>Correct entered data</p>;
+        return <p className={css.FormHeader}>{t("correctData")}</p>;
       default:
-        return <p className={css.FormHeader}>Add water</p>;
+        return <p className={css.FormHeader}>{t("addWater")}</p>;
     }
   };
+
+  const handleWaterAmountChange = (amount) => {
+    setWaterAmount(amount);
+    setValue("waterValue", amount.toString());
+  };
+
+  const isMinusButtonDisabled = waterAmount === 50;
+  const isPlusButtonDisabled = waterAmount === 5000;
+
   return (
-    <form className={css.WaterForm}>
+    <form className={css.WaterForm} onSubmit={handleSubmit(onSubmit)}>
       {FormHeader(operationType)}
-      <p className={css.AmountOfWater}>Amount of water</p>
+      <p className={css.AmountOfWater}>{t("waterAmount")}</p>
       <div className={css.TapAddWaterWrapper}>
-        <button type="button" className={css.TapAddWater}>
-          -
+        <button
+          type="button"
+          className={css.TapAddWater}
+          onClick={() => handleWaterAmountChange(Math.max(waterAmount - 50, 0))}
+          disabled={isMinusButtonDisabled}>
+          <svg>
+            <use xlinkHref={svgSprite + "#icon-remove"}></use>
+          </svg>
         </button>
-        <p className={css.TapAddWaterValue}>50ml</p>
-        <button type="button" className={css.TapAddWater}>
-          +
+        <p className={css.TapAddWaterValue}>
+          {waterAmount} {t("ml")}
+        </p>
+        <button
+          type="button"
+          className={css.TapAddWater}
+          onClick={() => handleWaterAmountChange(waterAmount + 50)}
+          disabled={isPlusButtonDisabled}>
+          <svg>
+            <use xlinkHref={svgSprite + "#icon-add"}></use>
+          </svg>
         </button>
       </div>
 
       <label className={css.RecordingTimeLabel}>
-        Recording time:
-        <input type="text" className={css.RecordingTime} />
+        {t("recordTime")}
+        <Controller
+          name="recordingTime"
+          control={control}
+          render={({field}) => (
+            <input
+              {...field}
+              type="text"
+              className={clsx(css.RecordingTime)}
+              placeholder="HH:MM"
+              onChange={(e) => {
+                const [newHours, newMinutes] = e.target.value.split(":");
+                setFormHours(newHours);
+                setFormMinutes(newMinutes);
+                field.onChange(e);
+              }}
+            />
+          )}
+        />
+        {errors.recordingTime && (
+          <p className={css.Error}>{errors.recordingTime.message}</p>
+        )}
       </label>
       <label className={css.WaterValueLabel}>
-        Enter the value of the water used:
-        <input type="number" className={css.WaterValue} />
+        {t("enterWaterValue")}
+        <Controller
+          name="waterValue"
+          control={control}
+          render={({field}) => (
+            <input
+              {...field}
+              type="number"
+              value={waterAmount || ""}
+              onChange={(e) => handleWaterAmountChange(Number(e.target.value))}
+              className={css.WaterValue}
+            />
+          )}
+        />
+        {errors.waterValue && (
+          <p className={css.Error}>{errors.waterValue.message}</p>
+        )}
       </label>
-      <button type="button" className={css.SaveBtn}>
-        Save
-      </button>
+      <button type="submit" className={css.SaveBtn}></button>
     </form>
   );
 };
+
 export default WaterForm;
--- src/components/WaterModal/WaterModal.jsx
+++ src/components/WaterModal/WaterModal.jsx
@@ -1,24 +1,86 @@
 import css from "./WaterModal.module.css";
 import WaterForm from "../WaterForm/WaterForm";
-import {useModal} from "../../hooks/useModal";
+import svgSprite from "../../assets/sprite.svg";
 
-const WaterModal = ({operationType}) => {
+const WaterModal = ({
+  operationType,
+  onClose,
+  water = {},
+  timestampFromUrl = "",
+}) => {
+  const handleClose = () => {
+    const id = setTimeout(() => {
+      onClose();
+      clearTimeout(id);
+    }, 300);
+  };
+
   const modalHeader = (operationType) => {
     switch (operationType) {
       case "add":
-        return "Add water";
+        return t("addWaterTitle");
       case "edit":
-        return "Edit the entered amount of water";
+        return t("editWaterAmount");
       default:
-        return "Add Water";
+        return t("addWaterTitle");
     }
   };
-  console.log(modalHeader(operationType));
+
+  const curentTimestamp = Number(timestampFromUrl);
+  const recordTimestamp = Number(water.date);
+
+  const editTime = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return curentTimestamp;
+      case "edit":
+        return recordTimestamp;
+    }
+  };
+
+  const waterPortion = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return 50;
+      case "edit":
+        return water.amount;
+      default:
+        return 50;
+    }
+  };
+
+  const waterID = (operationType) => {
+    switch (operationType) {
+      case "add":
+        return null;
+      case "edit":
+        return water.id;
+      default:
+        return null;
+    }
+  };
+
   return (
     <div className={css.WaterModal}>
       <h1>{modalHeader(operationType)}</h1>
-      <WaterForm operationType={operationType} />
+      <WaterForm
+        operationType={operationType}
+        editTime={editTime(operationType)}
+        waterPortion={waterPortion(operationType)}
+        waterID={waterID(operationType)}
+        handleClose={handleClose}
+      />
+      <button
+        type="button"
+        onClick={handleClose}
+        aria-label={t("closeWaterModal")}
+        className={css.WaterModalCloseBtn}>
+        <svg>
+          <use xlinkHref={svgSprite + "#icon-clear"}></use>
+        </svg>
+      </button>
     </div>
   );
 };
+
 export default WaterModal;
