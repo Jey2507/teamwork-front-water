3ececf5d718672c80ae8ca735fa15e57b32e8cb0
Myroslava Rogoza <myroslavarogoza@gmail.com>
2024-08-09 23:04:12 +0300

    modal positioning and scroll bar

--- src/components/UserSettingsModal/UserSettingsModal.jsx
+++ src/components/UserSettingsModal/UserSettingsModal.jsx
@@ -2,13 +2,14 @@ import css from "../UserSettingsModal/UserSettingsModal.module.css";
 import UserSettingsForm from "../UserSettingsForm/UserSettingsForm.jsx";
 import Modal from "../Modal/Modal";
 
-
 function UserSettingsModal() {
   return (
     <Modal>
-      <div className={css.settingsModal}>
-        <h2 className={css.settingsModalTitle}>Settings</h2>
-        <UserSettingsForm />
+      <div className={css.modalContainer}>
+        <div className={css.settingsModal}>
+          <h2 className={css.settingsModalTitle}>Settings</h2>
+          <UserSettingsForm />
+        </div>
       </div>
     </Modal>
   );
