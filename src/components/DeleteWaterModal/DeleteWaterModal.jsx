0f4888ad7a92afffb177bb6a69d4043bba88301e
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-08-10 15:37:19 +0300

    Update DeleteWaterModal.jsx

--- src/components/DeleteWaterModal/DeleteWaterModal.jsx
+++ src/components/DeleteWaterModal/DeleteWaterModal.jsx
@@ -3,27 +3,28 @@ import { deleteWaterEntry } from '../../redux/water/operations';
 import { closeModal } from '../../redux/ModalSlice';
 import toast from 'react-hot-toast';
 import Modal from '../Modal/Modal';
+import css from './DeleteWaterModal.module.css';
 
 const DeleteWaterModal = () => {
-    const dispatch = useDispatch();
-    const isModalOpen = useSelector((state) => state.modal.isModalOpen);
-    const entryId = useSelector((state) => state.modal.entryId);
-  
-    if (!isModalOpen) return null;
-  
-    const handleDelete = async () => {
-      try {
-        await dispatch(deleteWaterEntry(entryId)).unwrap();
-        dispatch(closeModal());
-        toast.success('Entry deleted successfully');
-      } catch (error) {
-        toast.error('Failed to delete entry');
-      }
-    };
-  
-    return (
-      <Modal>
-           <div className={css.modalContainer}>
+  const dispatch = useDispatch();
+  const isModalOpen = useSelector(state => state.modal.isModalOpen);
+  const entryId = useSelector(state => state.modal.entryId);
+
+  if (!isModalOpen) return null;
+
+  const handleDelete = async () => {
+    try {
+      await dispatch(deleteWaterEntry(entryId)).unwrap();
+      dispatch(closeModal());
+      toast.success('Entry deleted successfully');
+    } catch (error) {
+      toast.error('Failed to delete entry');
+    }
+  };
+
+  return (
+    <Modal>
+      <div className={css.modalContainer}>
         <div className={css.coverText}>
           <h2 className={css.title}>Delete entry</h2>
           <p className={css.text}>Are you sure you want to delete the entry?</p>
@@ -42,7 +43,6 @@ const DeleteWaterModal = () => {
 };
 
 export default DeleteWaterModal;
-
 
 /* const DeleteWaterModal = () => {
   const dispatch = useDispatch();
