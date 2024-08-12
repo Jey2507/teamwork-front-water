23c3dc5577a4e497fb52022797576a9767abc0f0
Dmytrii Tsybuliak <tonistark1997@gmail.com>
2024-08-13 02:32:32 +0300

    fixing calendar days for december

--- src/components/CalendarPagination/CalendarPagination.jsx
+++ src/components/CalendarPagination/CalendarPagination.jsx
@@ -1,6 +1,6 @@
 import css from './CalendarPagination.module.css';
 import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
-import { format, addMonths, subMonths, subYears, startOfMonth } from 'date-fns';
+import { format, addMonths, subMonths, startOfMonth } from 'date-fns';
 import { icons as sprite } from '../../assets/index.js';
 import { useDispatch, useSelector } from 'react-redux';
 import { selectMonth } from '../../redux/water/selectors.js';
@@ -11,32 +11,31 @@ export const CalendarPagination = ({ isActive, setIsActive }) => {
   const currentDate = useSelector(selectMonth);
   const monthQuery = new Date(currentDate.year, currentDate.month - 1);
   const monthNext = new Date(currentDate.year, currentDate.month);
-  // format(monthQuery, 'MMMM, yyyy')
-  // let monthQuery = currentDate.year + '-' + currentDate.month;
-
   const minDate = new Date('2020-01-01');
+
   const handlePrevMonth = () => {
-    let newMonth = subMonths(new Date(currentDate.year, currentDate.month), 1);
-    console.log(newMonth);
-    if (newMonth >= startOfMonth(minDate)) {
-      console.log('Down');
-      if (newMonth.getMonth() === 0) {
-        console.log('December');
-        newMonth = subYears(new Date(currentDate.year, 12), 1);
-        // newMonth.setMonth(11);
-      }
-      // dispatch(setCurrentDate({ year: 2024, month: 7 }));
+    let newMonth;
+    if (currentDate.month == 1) {
+      const setYear = currentDate.year - 1;
+      const setMonth = 12;
+      dispatch(setCurrentDate({ year: setYear, month: setMonth }));
+    } else {
+      newMonth = subMonths(new Date(currentDate.year, currentDate.month), 1);
       dispatch(setCurrentDate({ year: newMonth.getFullYear(), month: newMonth.getMonth() }));
     }
   };
   const handleNextMonth = () => {
     if (monthQuery < new Date()) {
-      console.log('Up');
-      const newMonth = addMonths(new Date(currentDate.year, currentDate.month), 1);
-      console.log(newMonth);
-      dispatch(setCurrentDate({ year: newMonth.getFullYear(), month: newMonth.getMonth() }));
+      let newMonth;
+      if (currentDate.month == 11) {
+        const setYear = currentDate.year;
+        const setMonth = 12;
+        dispatch(setCurrentDate({ year: setYear, month: setMonth }));
+      } else {
+        newMonth = addMonths(new Date(currentDate.year, currentDate.month), 1);
+        dispatch(setCurrentDate({ year: newMonth.getFullYear(), month: newMonth.getMonth() }));
+      }
     }
-    // console.log(monthQuery);
   };
   const isPrevDisabled = monthQuery <= startOfMonth(minDate);
   const isNextDisabled = monthNext > new Date();
--- src/components/WaterProgressBar/WaterProgressBar.jsx
+++ src/components/WaterProgressBar/WaterProgressBar.jsx
@@ -30,7 +30,7 @@ export default function WaterProgressBar() {
 
   if (!percentDaily || percentDaily < 0 || typeof percentDaily !== 'number') {
     percentDaily = 0;
-    console.log(percentDaily )
+    // console.log(percentDaily )
   } else if (percentDaily > 100) {
     percentDaily = 100;
   }
@@ -38,41 +38,26 @@ export default function WaterProgressBar() {
   let circleRightMod = -2;
 
   if (percentDaily <= 2) {
-    circleRightMod =  -10;
-  }
-  else if (percentDaily > 2 && percentDaily < 7) {
+    circleRightMod = -10;
+  } else if (percentDaily > 2 && percentDaily < 7) {
     circleRightMod = -8;
-  }
-  else if (percentDaily >= 7 && percentDaily < 10)
-  {
+  } else if (percentDaily >= 7 && percentDaily < 10) {
     circleRightMod = -6;
   }
 
-
   let floatPercentMod = -7;
 
-    if (percentDaily < 4 && percentDaily >= 0) {
-    floatPercentMod =  -3;
-  }
-  else if (percentDaily >= 4 && percentDaily < 12) {
+  if (percentDaily < 4 && percentDaily >= 0) {
+    floatPercentMod = -3;
+  } else if (percentDaily >= 4 && percentDaily < 12) {
     floatPercentMod = -4;
-  }
-  else if (percentDaily >= 12 && percentDaily < 13 )
-  {
+  } else if (percentDaily >= 12 && percentDaily < 13) {
     floatPercentMod = -5;
-  }
-    else if (percentDaily >= 13 && percentDaily < 20)
-  {
+  } else if (percentDaily >= 13 && percentDaily < 20) {
     floatPercentMod = -6;
-  }
-      else if (percentDaily >= 20 && percentDaily < 26)
-  {
+  } else if (percentDaily >= 20 && percentDaily < 26) {
     floatPercentMod = -6.5;
   }
-
-
-
-
 
   return (
     <div className={css.container}>
@@ -84,11 +69,11 @@ export default function WaterProgressBar() {
           style={{
             width: `${percentDaily}%`,
           }}
-         >
-           <div className={css.frontBarCircle} style={{right: circleRightMod}}>
-            <div className={css.floatPercent}
-              style={{ right: floatPercentMod }}
-            >{Math.round(percentDaily)}%</div>
+        >
+          <div className={css.frontBarCircle} style={{ right: circleRightMod }}>
+            <div className={css.floatPercent} style={{ right: floatPercentMod }}>
+              {Math.round(percentDaily)}%
+            </div>
           </div>
         </div>
       </div>
